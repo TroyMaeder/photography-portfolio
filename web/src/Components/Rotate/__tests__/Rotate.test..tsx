4a569ee7433b5b5c6ad0d07cef72fe3068ba909b
Troy Garraus <troy.garraus@b-2752.mynet>
2020-04-30 15:18:08 +0100

    test: for event firing and correctly adding classes

---
+++ web/src/Components/Rotate/__tests__/Rotate.test..tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import Rotate from '../Rotate'
+import Images from '../../../assets/apples'
+import { act } from 'react-dom/test-utils'
+
+describe('Rotate', () => {
+  it('updates image alt when wheel event is triggered', () => {
+    const map = {} as any
+    window.addEventListener = jest.fn((event, cb) => {
+      map[event] = cb
+    })
+
+    const component = mount(<Rotate images={Images} />)
+
+    act(() => {
+      map.wheel({ deltaY: 1 })
+    })
+
+    expect(component.find('img').prop('alt')).toMatch('Apple 1')
+  })
+
+  it('correctly adds a class', () => {
+   const component = mount(
+    <Rotate
+      className="apples-make-me-smile"
+      images={Images}
+    />)
+
+    expect(component.find('img').prop('className')).toMatch('apples-make-me-smile')
+  })
+})
