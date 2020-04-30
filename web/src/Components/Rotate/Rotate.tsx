06e9ba174dc2c7032e5f0e7e4a2d443ed8eeae20
Troy Garraus <troy.garraus@b-2752.mynet>
2020-04-30 16:03:12 +0100

    refactor: move wheel logic inside of `setCurrentImageIndex` callback

--- web/src/Components/Rotate/Rotate.tsx
+++ web/src/Components/Rotate/Rotate.tsx
@@ -7,35 +7,37 @@ interface Props {
 }
 
 const Rotate = ({ images, className }: Props) => {
-	let [, setCurrentImageIndex] = useState(0)
-	const currentImageIndex = useRef(0)
+  const [currentImageIndex, setCurrentImageIndex] = useState(0)
+  const length = useRef(images.length)
+  const pageShouldScroll = currentImageIndex === Number(length) - 1
+
+  if (pageShouldScroll) {
+    document.body.classList.add('scroll')
+  }
 
   useEffect(() => {
+    // https://dmitripavlutin.com/react-hooks-stale-closures/
     const handleWheel = (event: MouseWheelEvent): void => {
-      const isScrollingUp = event.deltaY > 0 && currentImageIndex.current < images.length - 1
-      const isScrollingDown = event.deltaY < 0 && currentImageIndex.current > 0
-      const pageShouldScroll = currentImageIndex.current === images.length -1
-
-      if (pageShouldScroll) {
-        document.body.classList.add('scroll')
-      }
-
-      if (isScrollingUp) {
-				setCurrentImageIndex(currentImageIndex.current += 1)
-      } else if (isScrollingDown) {
-        setCurrentImageIndex(currentImageIndex.current -= 1)
-      }
+      setCurrentImageIndex((currentImageIndex) => {
+        if (event.deltaY > 0) {
+          return Math.min(currentImageIndex + 1, length.current - 1)
+        } else {
+          return Math.max(currentImageIndex - 1, 0)
+        }
+      })
     }
 
     window.addEventListener('wheel', lodash.throttle(handleWheel, 100))
 
-		return () => window.removeEventListener('wheel', handleWheel)
-	}, [images.length])
+    return () => {
+      window.removeEventListener('wheel', handleWheel)
+    }
+  }, [])
 
   return (
     <img
-      alt={`apple ${currentImageIndex.current + 1}`}
-      src={images[currentImageIndex.current]}
+      alt={`apple ${currentImageIndex + 1}`}
+      src={images[currentImageIndex]}
       className={className}
     />
   )
